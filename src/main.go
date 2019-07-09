033c0323aef2fd8a52c69072f1b436f51d74b991
FantBoy <549003954@qq.com>
2019-07-09 23:07:20 +0800

    add tcp server

--- src/main.go
+++ src/main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"fmt"
+	. "github.com/FantBoy/go_tcp_server/src/libs"
+	"github.com/pkg/errors"
+)
+
+func server() error {
+	endpoint := NewTcpServer()
+
+	endpoint.AddHandleFunc("string", HandleStrings)
+	endpoint.AddHandleFunc("gob", HandleGob)
+
+	// 开始监听
+	return endpoint.Listen()
+}
+
+func main() {
+	err := server()
+	if err != nil {
+		fmt.Println("Error:", errors.WithStack(err))
+	}
+}
